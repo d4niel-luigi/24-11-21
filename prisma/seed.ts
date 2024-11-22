ddde08a58f64ffb9800aa83ae00e3de38d406bed
Petrik Diak <diak@petrik.hu>
2024-11-22 15:58:28 +0100

    lenn a kezme

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,21 +2,58 @@ import { PrismaClient } from '@prisma/client'
 import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient()
 
+async function main() {
+  for (let i = 0; i < 15; i++) {
+    await prisma.artist.create({
+      data: {
+        name: faker.music.artist(),
+        birthDate: faker.date.birthdate(),
+      }
+    })
+  }
 
+  for (let i = 0; i < 50; i++) {
+    const free = Math.random() < 0.25
+    await prisma.song.create({
+      data: {
+        artist: { connect: { id: faker.number.int({ min: 1, max: 15 }) } },
+        title: faker.music.songName(),
+        duration: faker.number.int({ min: 60, max: 150 }),
+        price: free ? 0 : faker.number.int({ min: 60, max: 150 }),
+        rating: faker.number.float({ min: 1, max: 5 })
+      }
+    })
+  }
+  for (let i = 0; i < 10; i++) {
+    await prisma.playlist.create({
+      data: {
+        name: faker.music.album(),
+        songs: {
+          connect: {
+            id: i + 1,
 
-async  function main() {
- for(let i = 0; i < 50; i++){
-  const free = Math.random() < 0.25
-  await prisma.song.create({
+          }
+        }
+      }
+    })
+  }
+  await prisma.playlist.update({
+    where: {
+      id: 2
+
+    },
     data: {
-      artist: faker.music.artist(),
-      title: faker.music.songName(),
-      duration: faker.number.int({min: 60, max: 150}),
-      price: free ? 0 : faker.number.int({min: 60, max: 150}),
-      rating: faker.number.float({min: 1, max: 5})
+      songs: {
+        connect: [
+          { id: 2 },
+          { id: 3 },
+          { id: 7 },
+          { id: 11 },
+          { id: 1 }
+        ]
+      }
     }
   })
- }
 }
 main()
   .then(async () => {
